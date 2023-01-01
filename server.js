3fb955443067de5cbb14220f8eaf73f37b093717
Sujay Mathane <sujaymathane@Sujays-MacBook-Air.local>
2023-01-01 20:27:02 +0530

    commint from mac

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require("express");
+var path=require("path");
+const app=express();
+
+app.get("/",function(req,res){
+    res.send("<h1>This is hello from 1st jan 2023</h1>")
+})
+
+app.get("/home",function(req,res){
+    res.sendFile(path.join(__dirname,"public/index.html"));
+})
+
+app.listen(3000);
+console.log("server is listening to port number 3000");
